7b88e6777d76ce4e1adef7afa74fe25b576ecd7f
vientokk <vientokk@gmail.com>
2023-10-04 00:29:37 +0900

    프런트 로그인 기능 구현

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -7,6 +7,5 @@ const ctrl = require("./home.ctrl");
 router.get("/", ctrl.output.home);
 router.get("/login", ctrl.output.login);
 router.post("/login", ctrl.process.login);
- 
 
 module.exports = router;
