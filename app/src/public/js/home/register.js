a7534d1060200943459ccddc3d2d34603242cb2b
vientokk <vientokk@gmail.com>
2023-10-10 06:28:43 +0900

    회원가입 요청 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -1,19 +1,24 @@
 "use strict";
 
 const id = document.querySelector("#id");
+const name = document.querySelector("#name");
 const psword = document.querySelector("#psword");
-const loginBtn = document.querySelector("#button");
+const confirmPsword = document.querySelector("#confirm-psword");
+const registerBtn = document.querySelector("#button");
 
-loginBtn.addEventListener("click",login);
+registerBtn.addEventListener("click",register);
 
-function login (){
+function register (){
     const req = {
         id: id.value,
+        name: name.value,
         psword: psword.value,
+        confirmPsword: confirmPsword.value,
     };
+ 
     // console.log(req);
     // console.log(JSON.stringify(req));
-    fetch("/login",{
+    fetch("/register",{
         method:"POST",
         headers:{
             "Content-Type": "application/json",
@@ -23,13 +28,13 @@ function login (){
     .then((res) => res.json())
     .then((res) => {
         if(res.success){
-            location.href = "/";
+            location.href = "/login";
         }else{
             alert(res.msg);
         }
     })
     .catch((err)=>{
-        console.error( "로그인 중 에러 발생");
+        console.error( "회원가입 중 에러 발생");
     });
 } 
 
