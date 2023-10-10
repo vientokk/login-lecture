c3b748b2d84aeb9ff11c0244f4c6305f9fbcb986
vientokk <vientokk@gmail.com>
2023-10-11 06:20:06 +0900

    회원가입 라우팅 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -9,32 +9,34 @@ const registerBtn = document.querySelector("#button");
 registerBtn.addEventListener("click",register);
 
 function register (){
+    if(!id.value) return alert("아이디를 입력해 주십시오");
+    if(psword.value !== confirmPsword.value) return alert("비밀번호가 일치하지 않습니다.");
+    
     const req = {
         id: id.value,
         name: name.value,
         psword: psword.value,
-        confirmPsword: confirmPsword.value,
     };
  
     // console.log(req);
     // console.log(JSON.stringify(req));
-    fetch("/register",{
-        method:"POST",
-        headers:{
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(req),
-    })
+    fetch("/register", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(req),
+  })
     .then((res) => res.json())
     .then((res) => {
-        if(res.success){
-            location.href = "/login";
-        }else{
-            alert(res.msg);
-        }
+      if (res.success) {
+        location.href = "/login";
+      } else {
+        alert(res.msg);
+      }
     })
-    .catch((err)=>{
-        console.error( "회원가입 중 에러 발생");
+    .catch((err) => {
+      console.error("회원가입 중 에러 발생");
     });
 } 
 
