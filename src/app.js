c8b568d30a7b14205ceb2bde8b51cdf90f9b4689
vientokk <vientokk@gmail.com>
2024-05-23 00:53:14 +0900

    프런트단 javascript  파일 만들기

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const home = require('./routes/home');
 //앱세팅
 app.set('views', "./views");
 app.set('view engine', 'ejs');
+app.use(express.static(`${__dirname}/public`));
 
 app.use("/", home); // use : 미들웨어를 등록해주는 메서드
 
