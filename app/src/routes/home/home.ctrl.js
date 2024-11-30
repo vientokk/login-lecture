d4448adbb59b2a9298569c1b12cc092fe6bfeed7
DEKA <deka@DEKAui-Macmini.local>
2024-11-30 15:30:11 +0900

    백그라운드에서 로그인 기능 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,14 +1,40 @@
 "use strict";
 
-const hello = ((req, res)=>{
-    res.render('home/index')
-});
+const users={
 
-const login = ((req, res)=>{
-    res.render('./home/login')
-});
+    id:["wooimIT", "나개발",, "김팀장"],
+    psword:["1234", "1234",";123456"],
+};
 
+const output={
+    hello : ((req, res)=>{
+        res.render('home/index')
+    }),
+    login : ((req, res)=>{
+        res.render('./home/login')
+    })
+}
+
+const process = {
+    login:(req,res)=>{
+        const id = req.body.id;
+        const psword = req.body.psword;
+        if(users.id.includes(id)){
+            const idx = users.id.indexOf(id);
+            if(users.psword[idx] === psword) {
+                return res.json(
+                    {success: true}
+                );
+            }
+        }
+        return res.json({
+            success:"false",
+            msg:"로그인에 실패했습니다.",
+        });
+    },
+};
+ 
 module.exports = {
-    hello, 
-    login,
+    output,
+    process,
 };
