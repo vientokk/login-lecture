3539aa30f01f1339f20f551eeaa3a92ed0e1acdb
DEKA <deka@DEKAui-Macmini.local>
2024-12-01 19:09:27 +0900

    UserStorage 모델구현

---
+++ app/src/models/UserStorage.js
@@ -0,0 +1,24 @@
+"use strict";
+
+class UserStorage{
+    static #users = {
+        id:["wooimIT", "나개발","김팀장"],
+        psword:["1234", "1234",";123456"],
+        name:["우리밋","나개발","김팅장"],
+    };    
+
+
+    static getUsers(...fields){
+        const users = this.#users;
+        const newUsers = fields.reduce((newUsers,field)=>{
+            if(users.hasOwnProperty(field)){
+                newUsers[field] = users[field];
+            }
+            return newUsers;
+        },{});
+        
+        return newUsers;
+    }
+}
+
+module.exports = UserStorage;
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,10 +1,6 @@
 "use strict";
-
-const users={
-
-    id:["wooimIT", "나개발",, "김팀장"],
-    psword:["1234", "1234",";123456"],
-};
+// const { use } = require(".");
+const UserStorage = require("../../models/UserStorage");
 
 const output={
     hello : ((req, res)=>{
@@ -19,18 +15,22 @@ const process = {
     login:(req,res)=>{
         const id = req.body.id;
         const psword = req.body.psword;
+
+        //const userStorage =  new UserStorage();
+        const users = UserStorage.getUsers("id", "psword");
+
+        const response = {};
+
         if(users.id.includes(id)){
             const idx = users.id.indexOf(id);
             if(users.psword[idx] === psword) {
-                return res.json(
-                    {success: true}
-                );
+                response.success = true;
+                return res.json(response);
             }
         }
-        return res.json({
-            success:false,
-            msg:"로그인에 실패했습니다.",
-        });
+        response.success = false;
+        response.msg = "로그인에 실패했습니다.";
+        return res.json(response);
     },
 };
  
